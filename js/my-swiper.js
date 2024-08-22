c85a08fca4cbee67ae09948fb000bfd422b5cf61
Maksym <112901851+Maksym82@users.noreply.github.com>
2024-08-22 22:02:57 +0300

    Create my-swiper.js

---
+++ js/my-swiper.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper('.my-swiper', {
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 0,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
