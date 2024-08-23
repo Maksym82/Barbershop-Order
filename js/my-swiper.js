3b867f269a98b8032b39c611a5701dbb1b8e330e
Maksym <112901851+Maksym82@users.noreply.github.com>
2024-08-23 15:09:20 +0300

    Update advantage

--- js/my-swiper.js
+++ js/my-swiper.js
@@ -3,7 +3,20 @@ var swiper = new Swiper('.my-swiper', {
   loop: true,
   slidesPerView: 1,
   spaceBetween: 0,
+  grabCursor: true,
+  
+  breakpoints: {
+    // when window width is >= 834px
+    834: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+      enabled: true,
+      
+    },
+     
+  },
 
+  
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
