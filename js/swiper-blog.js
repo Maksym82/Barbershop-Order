1b7755246d98bf59ea7f5f3bf70d1c3f33bc7319
Maksym <112901851+Maksym82@users.noreply.github.com>
2024-10-14 22:10:10 +0300

    Update styles blog

---
+++ js/swiper-blog.js
@@ -0,0 +1,32 @@
+
+const swiper = new Swiper('.swiper__blog', {
+    // Optional parameters
+    grabCursor: true,
+    loop: false,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+   
+    // Default parameters
+    slidesPerView: 1,
+    spaceBetween: 10,
+    // Responsive breakpoints
+    breakpoints: {
+      // when window width is >= 834px
+      834: {
+        slidesPerView: 2,
+        spaceBetween: 0,
+        enabled: false,
+      },
+      // when window width is >= 1440px
+      // 1440: {
+      //   slidesPerView: 6,
+      //   spaceBetween: 32,
+      // },
+    },
+  });
+  
